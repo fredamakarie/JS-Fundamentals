d8604fb674af8408a1ef7e009764244db5110cda
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-05-25 23:32:27 +0100

    Corrections to Tasks 3 and 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 let argnum = process.argv[2];
 let arg = Number(argnum);
 if (Number.isInteger(arg)){
-    console.log (`My Number:  ${arg}`);
+    console.log (`My number:  ${arg}`);
 } else{
     console.log("Not a number");
 }
